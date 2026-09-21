7857699d609367371cc07e33ca0e783976ee8488
이건탁 <wow2658@naver.com>
2026-09-21 22:30:16 +0900

    fix: Docker 패키지 실행 경로와 MySQL 연결 수정

--- app/main.py
+++ app/main.py
@@ -8,4 +8,4 @@ app = FastAPI()
 app.include_router(login_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
